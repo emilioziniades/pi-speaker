00b9d4dfdc13c38d3380178b88f9a4801cbb1e0e
Emilio Ziniades <emilioziniades@protonmail.com>
2022-02-07 21:21:34 +0200

    checking that bluetoothctl and pulseaudio are active

--- activate.sh
+++ activate.sh
@@ -10,8 +10,23 @@ bluetoothctl -- discoverable on
 bluetoothctl -- pairable on
 bluetoothctl -- agent on
 
+sudo systemctl status bluetooth > /dev/null
+
+if [ $? -eq 0 ] then
+    print_blue "bluetoothctl active..."
+else
+    print_red "bluetoothctl not active..."
+fi
+
 # start pulseaudio
 
 print_blue "starting pulseaudio..."
 pulseaudio --start
+pulseaudio --check
+if [ $? -eq 0 ] then
+    print_blue "pulse audio started..."
+else 
+    print_red "pulse audio not started..."
+fi
 
+# print three lines of systemctl status bluetoothctl
--- util.sh
+++ util.sh
@@ -1,6 +1,19 @@
 
 BLUE='\033[0;36m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
-print_blue() {
-    printf "${BLUE}$1${NC}\n"
+
+print_colour()
+{
+    printf "$(tput setaf $1)$2$(tput sgr 0)\n"
+}
+
+print_blue()
+{
+    print_colour 6 "$1"
+}
+
+print_red()
+{
+    print_colour 1 "$1"
 }
