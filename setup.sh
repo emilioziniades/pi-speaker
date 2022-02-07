72de7efd91bd62e8abc553ec2d06d40384d09686
Emilio Ziniades <emilioziniades@protonmail.com>
2022-02-07 18:52:41 +0200

    better config file insertion

--- setup.sh
+++ setup.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-config=".....\nClass = 0x41C\nDiscoverableTimeout = 0\n....."
+config="\n#.....( added by pi-speaker/setup.sh )\nClass = 0x41C\nDiscoverableTimeout = 0\n#....."
 bluetooth_dir=/etc/bluetooth
+bluetooth_config=$bluetooth_dir/main.conf
 
 # install dependencies
 
-echo "installing required packages for bluetooth music hub"
+echo "installing required packages for bluetooth music hub..."
 sudo apt-get install -y pulseaudio pulseaudio-module-bluetooth
 
 # create bluetooth group
 
-echo "creating bluetooth group and adding current user"
+echo "creating bluetooth group and adding current user..."
 sudo groupadd bluetooth
 sudo usermod -a -G bluetooth $(whoami)
 
 # add relevant configuration
 
-echo "adding config to /etc/bluetooth/main.conf"
+echo "adding config to /etc/bluetooth/main.conf..."
 sudo mkdir -p $bluetooth_dir
-echo -e $config | sudo tee -a $bluetooth_dir/main.conf > /dev/null
 
-echo "restarting bluetooth and rebooting device"
+cat $bluetooth_config | sed 's/\(\[General\]\)/\1\
+\
+# ..... (added by pi-speaker.sh)\
+Class = 0x41C\
+DiscoverableTimeout = 0\
+# ...../' > $bluetooth_config
+
+echo "restarting bluetooth service and rebooting device..."
 sudo systemctl restart bluetooth
 sudo reboot
