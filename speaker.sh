5d6e572c1b07fa5ad14c3ff946d27370e145eb7b
Emilio Ziniades <emilioziniades@protonmail.com>
2022-02-08 16:54:17 +0200

    consolidated all scripts into one, untested

--- speaker.sh
+++ speaker.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-source ./util.sh # contains print_blue and print_red
+####### from util.sh
+
+print_colour()
+{
+    printf "$(tput setaf $1)$2$(tput sgr 0)\n"
+}
+
+print_blue()
+{
+    print_colour 6 "$1"
+}
+
+print_red()
+{
+    print_colour 1 "$1"
+}
+
+######## from setup.sh
 
 bluetooth_dir=/etc/bluetooth
 bluetooth_config=$bluetooth_dir/main.conf
@@ -15,7 +32,6 @@ read -p "Enter name of device: " NAME
 read -p "Enter pin: " PIN
 echo -e "*\t$PIN" | sudo tee $pin_file > /dev/null
 sudo chmod 600 $pin_file
-
 
 # install dependencies
 
@@ -49,7 +65,7 @@ EOF
 
 # disable avrcp so that connected device can control volume
 # TODO : should use $bluetooth_service in /etc/systemd/system, but not working for some reason
-sudo sed -i 's/\(ExecStart.*\)/\1 --noplugin=avrcp/' /lib/systemd/system/bluetooth.service
+sudo sed -i 's/\(ExecStart.*\)/\1 --noplugin=avrcp,sap/' /lib/systemd/system/bluetooth.service
 
 ##### bluez-tools config
 print_blue "adding config to $bluez_tools_service..."
@@ -72,3 +88,44 @@ EOF
 print_blue "restarting bluetooth service and rebooting device..."
 sudo systemctl restart bluetooth
 sudo reboot
+
+##### from activate .sh
+
+# activate bluetoothctl
+
+print_blue "activating bluetoothctl agent..."
+bluetoothctl -- power on
+bluetoothctl -- discoverable on
+bluetoothctl -- pairable on
+bluetoothctl -- agent on
+
+sudo systemctl status bluetooth > /dev/null
+
+if [ $? -eq 0 ]; then
+    print_blue "bluetoothctl active..."
+else
+    print_red "bluetoothctl not active..."
+fi
+
+# start pulseaudio
+
+print_blue "starting pulseaudio..."
+pulseaudio --start
+pulseaudio --check
+if [ $? -eq 0 ]; then
+    print_blue "pulse audio started..."
+else 
+    print_red "pulse audio not started..."
+fi
+
+
+# launch pulseaudio on boot and configure autoload
+
+systemctl --user enable pulseaudio
+print_blue "pulseaudio will launch on startup"
+
+sudo systemctl enable bt-agent
+print_blue "bt-agent (bluez-tools) will launch on startup"
+
+sudo raspi-config nonint do_boot_behaviour "B2 Console Autologin"
+print_blue "raspi-config edited: console will autologin on startup"
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat test 
-echo -e '\n'
-sed 's/\(ExecStart.*\)/\1 --noplugin=avrcp/' test
-echo -e '\n'
